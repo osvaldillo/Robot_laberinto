30bfa445636197d493f6a18b3df1d76995a8a248
Osvaldo Alferez <osvaldoalferez@gmail.com>
2022-10-20 14:48:15 -0500

    v.02.1

--- Robot_ingenierias-laberinto.ino
+++ Robot_ingenierias-laberinto.ino
@@ -1,29 +1,33 @@
 
 //puto
-const int EchoPin1 = 5;
-const int TriggerPin1 = 6;
+const int Echo = 5;
+const int Trigger = 6;
 
 
 void setup() {
   // put your setup code here, to run once:
-      
-  pinMode(TriggerPin1, OUTPUT);
-  pinMode(EchoPin1, INPUT);
+  Serial.begin(9600);
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo, INPUT);
+  digitalWrite(Trigger, LOW);
+  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int cm1 = ping1(TriggerPin1, EchoPin1);
-  int ping(int TriggerPin1, int EchoPin1) {
-  long duration1, distanceCm1;
-  digitalWrite(TriggerPin1, LOW);  //para generar un pulso limpio ponemos a LOW 4us
-  delayMicroseconds(4);
-  digitalWrite(TriggerPin1, HIGH);  //generamos Trigger (disparo) de 10us
+  long t;
+  long d;
+
+  digitalWrite(Trigger, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TriggerPin1, LOW);
-  duration1 = pulseIn(EchoPin1, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
-  distanceCm1 = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
-  return distanceCm1;
+  digitalWrite(Trigger, LOW);
+  t = pulseIn(Echo, HIGH);
+  d = t/59 ;
+  Serial.print("Distancia: ");
+  Serial.print(d);
+  Serial.println(" cm");
+  delay(100);
+  
   
 }
