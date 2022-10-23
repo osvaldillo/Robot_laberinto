38b68f0b9d71058d059a7dc924dcef4b07422281
Osvaldo Alferez <osvaldoalferez@gmail.com>
2022-10-22 22:39:11 -0500

    v.03 Implementacion del algoritmo de la mano derecha e implementacion de funciones auxiliares en archivo Roboto.h

--- Robot_ingenierias-laberinto.ino
+++ Robot_ingenierias-laberinto.ino
@@ -1,34 +1,34 @@
 #include "Roboto.h"
 
- 
-//putototo
-const int Echo = 5;
-const int Trigger = 6;
+int sensorFrontal = 1;
+int sensorLateral = 2;
+int M1R = 3;  //Motor izquierdo, avance hacia adelante
+int M1L = 4;  //Motor izquierdo, avance hacia atrás
+int M2R = 5;  //Motor derecho, avance hacia adelante
+int M2L = 6;  //Motor derecho, avance hacia atras
 
+const int radius;  //radio de giro del robot
+const int wheelRadius; //radio de la llanta
+const int rpm; //velocidad angular de los motores
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(Trigger, OUTPUT);
-  pinMode(Echo, INPUT);
-  digitalWrite(Trigger, LOW);
-  dely();
-
+  bool fordWard = digitalRead(sensorFrontal);
+  bool sideWard= digitalRead(sensorLateral);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  long t;
-  long d;
-  
-  digitalWrite(Trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(Trigger, LOW);
-  t = pulseIn(Echo, HIGH);
-  d = t/59 ;
-  Serial.print("Distancia: ");
-  Serial.print(d);
-  Serial.println(" cm");
-  delay(100);
-  
+  if(fordWard == false){                          //Si no hay obstáculos...
+    walk(M1R, M2R);                                 //avanza
+    if(sideWard == false){                          //Si no hay pared derecha...
+      pause(M1R, M2R);                                //detente
+      turnR(M1R, M2L, radius,wheelRadius, rpm);       //gira a la derecha y empieza de nuevo
+    }
+  }else{                                          //Si hay un obstáculo...
+    pause(M1R, M2R);                                //detente
+    if(sideWard == false){                          //Si no hay pared derecha...
+      turnR(M1R, M2L, radius,wheelRadius, rpm);       //gira a la derecha y empieza de nuevo
+    }else{                                          //Si hay pared a la derecha
+      turnL(M1L, M2R, radius,wheelRadius, rpm);       //gira a la izquierda y empieza de nuevo
+    }
+  }
 }
--- Roboto.h
+++ Roboto.h
@@ -1,5 +1,4 @@
- 
-bool fordWard(int Echo, int Trigger){
+/*bool fordWard(int Echo, int Trigger){
   //código para determinar si hay muro al frente
   long t;
   long d;
@@ -9,26 +8,59 @@ bool fordWard(int Echo, int Trigger){
   digitalWrite(Trigger, LOW);
   t = pulseIn(Echo, HIGH);
   d = t/59 ;
-  return d<3
+  return d<3;
 }
 
-bool  sideWard(){
+
+
+bool  sideWard(int Echo, int Trigger){
   //codigo para determinar si hay muro a la derecha
-}
+  long t;
+  long d;
+  
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trigger, LOW);
+  t = pulseIn(Echo, HIGH);
+  d = t/59 ;
+  return d<3;
+}*/
 
-void turnR(){
+void turnR(int M1R, int M2L, int radius, int wheelRadius, int speed){
   //código para girar a la derecha
+  
+  const float pi = 3.141592;
+  t = (radius*pi/(2*wheelRadius))/speed  //cálculo del tiempo que tardará en girar noventa grados a la derecha
+  digitalWrite(M1R, HIGH);
+  digitalWrite(M2L, HIGH);
+  delay(t);
+  digitalWrite(M1R, LOW);
+  digitalWrite(M2L, LOW);
 }
 
-void turnL(){
+void turnL(int M1L, int M2R, int radius, int wheelRadius, int speed){
   //código para girar a la izquierda
+
+  float pi = 3.141592;
+  t = (radius*pi/(2*wheelRadius))/speed  //cálculo del tiempo que tardará en girar noventa grados a la derecha
+  digitalWrite(M1L, HIGH);
+  digitalWrite(M2R, HIGH);
+  delay(t);
+  digitalWrite(M1L, LOW);
+  digitalWrite(M2R, LOW);
 }
 
-void walk(){
+void walk(int MIR, int M2R){
   //código para avanzar hacia el frente
+  digitalWrite(M1R, HIGH);
+  digitalWrite(M2R, HIGH);
+  
 }
 
-void pause(){
+void pause(int MIR, int M2R){
   //código para detenerse
+  digitalWrite(M1R, LOW);
+  digitalWrite(M2R, LOW);
 }
+
   
